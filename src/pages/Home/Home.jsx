37c79df789835d1886fa31929b2e6b4178af88b6
Marynryn <murmar95@gmail.com>
2023-12-25 13:40:23 +0200

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,10 @@
 
-const styles = {
-    container: {
-        minHeight: 'calc(100vh - 50px)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background: "#f69a73",
-    },
-    title: {
-        fontWeight: 500,
-        fontSize: 48,
-        textAlign: 'center',
-    },
-
-
-
-};
+import css from "./Home.module.css"
 
 export default function Home() {
     return (
-        <div style={styles.container}>
-            <h1 style={styles.title}>
+        <div style={css.container}>
+            <h1 style={css.title}>
                 Welcome to your phonebook{' '}
                 <span role="img" aria-label="Greeting icon">
                     💁‍♀️

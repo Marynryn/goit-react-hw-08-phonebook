7160c18538e9be87480d6357282a7d2ad28502d4
Marynryn <132609093+Marynryn@users.noreply.github.com>
2023-12-25 21:58:44 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -61,8 +61,7 @@ const RegisterForm = () => {
                             <input
                                 type="text"
                                 name="password"
-                                pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-                                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                               
 
                                 id={passwordId}
                                 required
